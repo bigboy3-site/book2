6ead8b73c0741964057733c57a606e4fe6c8cfa1
bigboy3-site <bigboy3@inbox.ru>
2020-05-09 15:58:03 +0400

    Adding aditionl tools

--- README.md
+++
@@ -1,3 +0,0 @@
-Update and create book2 web site 
-Note: it will be responsive and html, css, bootstrap
-
---
+++ js/main.js
@@ -0,0 +1,12 @@
+ /** Transparent Navigatio bars */
+
+ let navbar = $(".navbar");
+
+$(window).scroll(function(){
+  let oTop = $(".section-2").offset().top - window.innerHeight;
+  if($(window).scrollTop() > oTop){
+    navbar.addClass("sticky");
+  }else{
+    navbar.removeClass("sticky");
+  }
+});
